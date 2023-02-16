f3f51d4bc6115451d30d31dffd0180a9a33b150c
wjdals0509 <wjdals050932@gmail.com>
2023-02-16 13:43:22 +0900

    updated (movie-list)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import { useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    //함수를 사용해서 toDo를 수정
-    //state(toDo,toDos)를 직접적으로 수정하지X
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
   return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
